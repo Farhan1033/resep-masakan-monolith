0e34b265e2d501cb167a403f42bc738e43d76564
Farhan1033 <hanzoi1006@gmail.com>
2025-09-15 15:44:39 +0700

    Pembuatan package error

---
+++ pkg/errs/error.go
@@ -0,0 +1,43 @@
+package errs
+
+import "net/http"
+
+type ErrMessage interface {
+	Message() string
+	StatusCode() int
+	Error() string
+}
+
+type ErrMessageData struct {
+	ErrMessage    string `json:"message" example:"This is an error message"`
+	ErrStatusCode int    `json:"status_code" example:"400"`
+	ErrError      string `json:"error" example:"BAD_REQUEST"`
+}
+
+func (e *ErrMessageData) Message() string {
+	return e.ErrMessage
+}
+
+func (e *ErrMessageData) StatusCode() int {
+	return e.ErrStatusCode
+}
+
+func (e *ErrMessageData) Error() string {
+	return e.ErrError
+}
+
+func NewInternalServerError(message string) ErrMessage {
+	return &ErrMessageData{
+		ErrMessage:    message,
+		ErrStatusCode: http.StatusInternalServerError,
+		ErrError:      "INTERNAL_SERVER_ERROR",
+	}
+}
+
+func NewNotFound(message string) ErrMessage {
+	return &ErrMessageData{
+		ErrMessage:    message,
+		ErrStatusCode: http.StatusNotFound,
+		ErrError:      "DATA_NOT_FOUND",
+	}
+}
