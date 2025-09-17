6a621c5dd8d94fd1506d753e2fc1f2629babe321
Farhan1033 <hanzoi1006@gmail.com>
2025-09-18 01:02:36 +0700

    Penambahan postgres db dan fungsi untuk pemanggilan file .env serta inisiasi auth repository, service dan handler

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,31 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/Farhan1033/resep-masakan-monolith.git/infra/config"
+	postgressql "github.com/Farhan1033/resep-masakan-monolith.git/infra/postgres"
+	authhandler "github.com/Farhan1033/resep-masakan-monolith.git/internal/auth_module/handler"
+	authrepositorypg "github.com/Farhan1033/resep-masakan-monolith.git/internal/auth_module/repository/auth_repository_pg"
+	authserviceimpl "github.com/Farhan1033/resep-masakan-monolith.git/internal/auth_module/service/auth_service_impl"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	config.LoadEnv()
+	postgressql.InitPostgres()
+
 	r := gin.Default()
+
+	// Init Repository
+	authRepo := authrepositorypg.NewAuthRepository(postgressql.DB)
+
+	// Init Service
+	authService := authserviceimpl.NewAuthService(authRepo)
+
+	// Setup Router
+	publicGroup := r.Group("/api/v1")
+
+	// Init Handler
+	authhandler.NewAuthHandler(publicGroup, authService)
 
 	r.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{"Status": "Berhasil"})
