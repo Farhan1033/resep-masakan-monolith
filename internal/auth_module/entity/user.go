1a101a8893588f59ed31c48f8009722e82118e4f
Farhan1033 <hanzoi1006@gmail.com>
2025-09-18 01:04:17 +0700

    Modifikasi entity user untuk pengecekan waktu pembuatan akun dan update akun

--- internal/auth_module/entity/user.go
+++ internal/auth_module/entity/user.go
@@ -1,15 +1,19 @@
 package entity
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type User struct {
-	ID       uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
-	Name     string    `gorm:"type:varchar(30);not null" json:"name"`
-	Email    string    `gorm:"type:varchar(30);not null;unique" json:"email"`
-	Password string    `gorm:"type:varchar(100);not null" json:"-"`
+	ID        uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
+	Name      string    `gorm:"type:varchar(30);not null" json:"name"`
+	Email     string    `gorm:"type:varchar(30);not null;unique" json:"email"`
+	Password  string    `gorm:"type:varchar(100);not null" json:"-"`
+	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt time.Time `gorm:"autoCreateTime;autoUpdateTime" json:"updated_at"`
 }
 
 func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
