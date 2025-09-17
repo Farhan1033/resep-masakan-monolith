66529a318b2d052901aeed072bc4e3534f80816b
Farhan1033 <hanzoi1006@gmail.com>
2025-09-18 01:03:17 +0700

    Pembuatan koneksi ke database postgres

---
+++ infra/postgres/postgres.go
@@ -0,0 +1,38 @@
+package postgressql
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/Farhan1033/resep-masakan-monolith.git/infra/config"
+	"github.com/Farhan1033/resep-masakan-monolith.git/internal/auth_module/entity"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	DB  *gorm.DB
+	err error
+)
+
+func InitPostgres() {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta",
+		config.GetKey("DB_HOST"),
+		config.GetKey("DB_USER"),
+		config.GetKey("DB_PASS"),
+		config.GetKey("DB_NAME"),
+		config.GetKey("DB_PORT"),
+	)
+
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database: ", err)
+	}
+
+	errMigrate := DB.AutoMigrate(entity.User{})
+	if errMigrate != nil {
+		log.Fatal("Failed to migrate database")
+	}
+
+	fmt.Println("✅ Database connected successfully")
+}
