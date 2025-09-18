e0cbb00016f6bfa1254fd92db6ea896dd3ac475d
Farhan1033 <hanzoi1006@gmail.com>
2025-09-18 15:31:29 +0700

    Pembuatan entitas kategori

---
+++ internal/category_module/entity/categroy.go
@@ -0,0 +1,27 @@
+package categoryentity
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type Category struct {
+	ID        uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
+	Name      string    `gorm:"type:varchar(100);not null" json:"name"`
+	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt time.Time `gorm:"autoCreateTime;autoUpdateTime" json:"updated_at"`
+}
+
+func (c *Category) BeforeCreate(tx *gorm.DB) (err error) {
+	if c.ID == uuid.Nil {
+		c.ID = uuid.New()
+	}
+
+	return
+}
+
+func (Category) TableName() string {
+	return "category"
+}
