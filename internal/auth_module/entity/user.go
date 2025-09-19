65e83e97b5292ef62dd659eb6c8f0f30d6f347ab
Farhan1033 <hanzoi1006@gmail.com>
2025-09-20 01:39:08 +0700

    Perubahan entity tabel users

--- internal/auth_module/entity/user.go
+++ internal/auth_module/entity/user.go
@@ -9,9 +9,12 @@ import (
 
 type User struct {
 	ID        uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
-	Name      string    `gorm:"type:varchar(30);not null" json:"name"`
 	Email     string    `gorm:"type:varchar(30);not null;unique" json:"email"`
+	UserName  string    `gorm:"type:varchar(100);not null;unique" json:"user_name"`
+	FullName  string    `gorm:"type:varchar(255);not null" json:"full_name"`
 	Password  string    `gorm:"type:varchar(100);not null" json:"-"`
+	AvatarUrl string    `gorm:"type:text" json:"avatar_url"`
+	Bio       string    `gorm:"type:text" json:"bio"`
 	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
 	UpdatedAt time.Time `gorm:"autoCreateTime;autoUpdateTime" json:"updated_at"`
 }
