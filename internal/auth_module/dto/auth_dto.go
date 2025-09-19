8f30123bd1780455081b956578639ed5c33f131d
Farhan1033 <hanzoi1006@gmail.com>
2025-09-20 01:38:53 +0700

    Perubahan kode dto pada saat request register dan update serta response untuk register dan login

--- internal/auth_module/dto/auth_dto.go
+++ internal/auth_module/dto/auth_dto.go
@@ -3,15 +3,17 @@ package dto
 import "github.com/google/uuid"
 
 type CreateRequest struct {
-	Name     string `json:"name" validate:"required,min=4,max=30"`
 	Email    string `json:"email" validate:"required,email"`
+	UserName string `json:"user_name" validate:"required,min=4"`
+	FullName string `json:"full_name" validate:"required,min=4"`
 	Password string `json:"password" validate:"required,min=6,max=30"`
 }
 
 type UpdateRequest struct {
-	Name     string `json:"name,omitempty" validate:"omitempty,min=4,max=30"`
-	Email    string `json:"email,omitempty" validate:"omitempty,email"`
-	Password string `json:"password,omitempty" validate:"omitempty,min=6,max=30"`
+	Email     string `json:"email,omitempty" validate:"omitempty,email"`
+	FullName  string `json:"full_name,omitempty" validate:"omitempty,min=4,max=30"`
+	AvatarUrl string `json:"avatar_url,omitempty" validate:"omitempty;url"`
+	Bio       string `json:"bio,omitempty" validate:"omitempty,"`
 }
 
 type LoginRequest struct {
@@ -20,12 +22,21 @@ type LoginRequest struct {
 }
 
 type CreateAuthResponse struct {
-	ID    uuid.UUID `json:"id"`
-	Name  string    `json:"name"`
-	Email string    `json:"email"`
+	ID        uuid.UUID `json:"id"`
+	Email     string    `json:"email"`
+	UserName  string    `json:"user_name"`
+	FullName  string    `json:"full_name"`
+	CreatedAt string    `json:"created_at"`
 }
 
 type LoginResponse struct {
-	Message string `json:"message"`
-	Token   string `json:"token"`
+	Data  *LoginDataResponse `json:"data"`
+	Token any                `json:"token"`
+}
+
+type LoginDataResponse struct {
+	ID       uuid.UUID `json:"id"`
+	Email    string    `json:"email"`
+	UserName string    `json:"user_name"`
+	FullName string    `json:"full_name"`
 }
