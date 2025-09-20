614631e8e4aab0cfcea6882e901065a9aeb536df
Farhan1033 <hanzoi1006@gmail.com>
2025-09-21 01:36:17 +0700

    Implementasi repository interfaces untuk ingredient module

---
+++ internal/ingredient_module/repository/ingredient_repository_pg/repository_pg.go
@@ -0,0 +1,69 @@
+package ingredientrepositorypg
+
+import (
+	"fmt"
+
+	ingrediententity "github.com/Farhan1033/resep-masakan-monolith.git/internal/ingredient_module/entity"
+	ingredientrepository "github.com/Farhan1033/resep-masakan-monolith.git/internal/ingredient_module/repository/ingredient_repository"
+	"github.com/Farhan1033/resep-masakan-monolith.git/pkg/errs"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type IngredientRepo struct {
+	db *gorm.DB
+}
+
+func NewIngredientRepository(db *gorm.DB) ingredientrepository.IngredientRepository {
+	return &IngredientRepo{
+		db: db,
+	}
+}
+
+func (r *IngredientRepo) Create(ingredient *ingrediententity.Ingredient) (*ingrediententity.Ingredient, errs.ErrMessage) {
+	if err := r.db.Create(ingredient).Error; err != nil {
+		return nil, errs.NewInternalServerError(err.Error())
+	}
+
+	return ingredient, nil
+}
+
+func (r *IngredientRepo) Get() ([]ingrediententity.Ingredient, errs.ErrMessage) {
+	var payload []ingrediententity.Ingredient
+
+	if err := r.db.Where("is_active = true").Order("name ASC").Find(&payload).Error; err != nil {
+		return nil, errs.NewNotFound("Data not found!")
+	}
+
+	return payload, nil
+}
+
+func (r *IngredientRepo) GetByName(name string) (*ingrediententity.Ingredient, errs.ErrMessage) {
+	var payload ingrediententity.Ingredient
+
+	if err := r.db.Where("name = ?", name).First(&payload).Error; err != nil {
+		return nil, errs.NewNotFound(fmt.Sprintf("This ingredient with %s not found!", name))
+	}
+
+	return &payload, nil
+}
+
+func (r *IngredientRepo) GetById(id uuid.UUID) (*ingrediententity.Ingredient, errs.ErrMessage) {
+	var payload ingrediententity.Ingredient
+
+	if err := r.db.Where("id = ?", id).First(&payload).Error; err != nil {
+		return nil, errs.NewNotFound("This ingredient not found or inactive")
+	}
+
+	return &payload, nil
+}
+
+func (r *IngredientRepo) Delete(id uuid.UUID, status bool) errs.ErrMessage {
+	if err := r.db.Model(&ingrediententity.Ingredient{}).
+		Where("id = ?", id).
+		Update("is_active", status).Error; err != nil {
+		return errs.NewInternalServerError(err.Error())
+	}
+
+	return nil
+}
