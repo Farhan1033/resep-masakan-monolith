e33c2b9a36d2f3593c156b48c977cf27fec91cad
Farhan1033 <hanzoi1006@gmail.com>
2025-09-21 01:33:27 +0700

    Penambahan inisialisasi ingredient module

--- cmd/main.go
+++ cmd/main.go
@@ -9,6 +9,9 @@ import (
 	categoryhandler "github.com/Farhan1033/resep-masakan-monolith.git/internal/category_module/handler"
 	categoryrepositorypg "github.com/Farhan1033/resep-masakan-monolith.git/internal/category_module/repository/category_repository_pg"
 	categoryserviceimp "github.com/Farhan1033/resep-masakan-monolith.git/internal/category_module/service/category_service_imp"
+	ingredienthandler "github.com/Farhan1033/resep-masakan-monolith.git/internal/ingredient_module/handler"
+	ingredientrepositorypg "github.com/Farhan1033/resep-masakan-monolith.git/internal/ingredient_module/repository/ingredient_repository_pg"
+	ingredientserviceimp "github.com/Farhan1033/resep-masakan-monolith.git/internal/ingredient_module/service/ingredient_service_imp"
 	"github.com/Farhan1033/resep-masakan-monolith.git/pkg/middleware"
 	"github.com/gin-gonic/gin"
 )
@@ -22,10 +25,12 @@ func main() {
 	// Init Repository
 	authRepo := authrepositorypg.NewAuthRepository(postgressql.DB)
 	categoryRepo := categoryrepositorypg.NewCategoryRepository(postgressql.DB)
+	ingredientRepo := ingredientrepositorypg.NewIngredientRepository(postgressql.DB)
 
 	// Init Service
 	authService := authserviceimpl.NewAuthService(authRepo)
 	categoryService := categoryserviceimp.NewCategoryService(categoryRepo)
+	ingredientService := ingredientserviceimp.NewIngredientService(ingredientRepo)
 
 	// Setup Router
 	publicGroup := r.Group("/api/v1")
@@ -35,6 +40,7 @@ func main() {
 	// Init Handler
 	authhandler.NewAuthHandler(publicGroup, authService)
 	categoryhandler.NewCategoryHandler(privateGroup, categoryService)
+	ingredienthandler.NewIngredientHandler(privateGroup, ingredientService)
 
 	r.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{"Status": "Berhasil"})
