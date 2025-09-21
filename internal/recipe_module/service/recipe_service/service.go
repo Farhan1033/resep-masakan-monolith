fabb6b34b3c1ad7b818af509d4063696354702e9
Farhan1033 <hanzoi1006@gmail.com>
2025-09-22 01:04:51 +0700

    Pembuatan recipe service

---
+++ internal/recipe_module/service/recipe_service/service.go
@@ -0,0 +1,15 @@
+package recipeservice
+
+import (
+	"github.com/Farhan1033/resep-masakan-monolith.git/internal/recipe_module/dto"
+	"github.com/Farhan1033/resep-masakan-monolith.git/pkg/errs"
+	"github.com/google/uuid"
+)
+
+type RecipeService interface {
+	Create(payload *dto.CreateRequest, IdUser uuid.UUID) (*dto.CreateResponse, errs.ErrMessage)
+	GetByPagination(page, limit int) (*dto.PaginatedResponse, errs.ErrMessage)
+	GetById(id uuid.UUID) (*dto.GetResponse, errs.ErrMessage)
+	Update(id uuid.UUID, payload *dto.UpdateRequest, userId uuid.UUID) (*dto.UpdateResponse, errs.ErrMessage)
+	Delete(id uuid.UUID, status bool) errs.ErrMessage
+}
