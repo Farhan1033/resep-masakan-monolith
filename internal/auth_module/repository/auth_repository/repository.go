f8111a1b4c45651eef621c5f6c77f6bf810fc6ca
Farhan1033 <hanzoi1006@gmail.com>
2025-09-21 17:22:48 +0700

    Penambahan repository untuk get id user

--- internal/auth_module/repository/auth_repository/repository.go
+++ internal/auth_module/repository/auth_repository/repository.go
@@ -3,9 +3,11 @@ package authrepository
 import (
 	"github.com/Farhan1033/resep-masakan-monolith.git/internal/auth_module/entity"
 	"github.com/Farhan1033/resep-masakan-monolith.git/pkg/errs"
+	"github.com/google/uuid"
 )
 
 type AuthRepostiory interface {
 	CreateUser(user *entity.User) (*entity.User, errs.ErrMessage)
 	GetByEmail(email string) (*entity.User, errs.ErrMessage)
+	GetById(id uuid.UUID) (*entity.User, errs.ErrMessage)
 }
