43d62a81c9f54692a4ae1b6beb1d7e055dab7d4d
Farhan1033 <hanzoi1006@gmail.com>
2025-10-02 00:23:10 +0700

    Pembuatan service interfaces detail recipe

---
+++ internal/detail_recipe_module/service/detail_recipe_service/service.go
@@ -0,0 +1,16 @@
+package detailrecipeservice
+
+import (
+	"github.com/Farhan1033/resep-masakan-monolith.git/internal/detail_recipe_module/dto"
+	"github.com/Farhan1033/resep-masakan-monolith.git/pkg/errs"
+	"github.com/google/uuid"
+)
+
+type DetailRecipeService interface {
+	CreateDetailRecipe(id uuid.UUID, req *dto.CreateDetailRecipeRequest) (*dto.DetailRecipeResponse, errs.ErrMessage)
+	GetAllDetailRecipes() ([]dto.RecipeWithIngredientsResponse, errs.ErrMessage)
+	GetDetailRecipeById(id uuid.UUID) (*dto.RecipeWithIngredientsResponse, errs.ErrMessage)
+	GetDetailRecipeByRecipeId(recipeId uuid.UUID) (*dto.RecipeWithIngredientsResponse, errs.ErrMessage)
+	UpdateDetailRecipe(id uuid.UUID, req *dto.UpdateDetailRecipeRequest) errs.ErrMessage
+	DeleteDetailRecipe(id uuid.UUID) errs.ErrMessage
+}
