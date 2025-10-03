eb601c0dd236a8edc27c992655369192f6592b8a
Farhan1033 <hanzoi1006@gmail.com>
2025-10-04 01:29:36 +0700

    Perubahan interfaces di detail_recipe service

--- internal/detail_recipe_module/service/detail_recipe_service/service.go
+++ internal/detail_recipe_module/service/detail_recipe_service/service.go
@@ -9,7 +9,7 @@ import (
 type DetailRecipeService interface {
 	CreateDetailRecipe(id uuid.UUID, req *dto.CreateDetailRecipeRequest) (*dto.DetailRecipeResponse, errs.ErrMessage)
 	GetAllDetailRecipes() ([]dto.RecipeWithIngredientsResponse, errs.ErrMessage)
-	GetDetailRecipeById(id uuid.UUID) (*dto.RecipeWithIngredientsResponse, errs.ErrMessage)
+	GetDetailRecipeById(id uuid.UUID) (*dto.DetailRecipeResponse, errs.ErrMessage)
 	GetDetailRecipeByRecipeId(recipeId uuid.UUID) (*dto.RecipeWithIngredientsResponse, errs.ErrMessage)
 	UpdateDetailRecipe(id uuid.UUID, req *dto.UpdateDetailRecipeRequest) errs.ErrMessage
 	DeleteDetailRecipe(id uuid.UUID) errs.ErrMessage
