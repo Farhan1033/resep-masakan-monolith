66e98c98299142990247cb37074aa69efc2a7181
Farhan1033 <hanzoi1006@gmail.com>
2025-10-03 23:53:29 +0700

    Pembuatan repository interfaces untuk recipe step

---
+++ internal/recipe_steps_module/repository/step_repository/repository.go
@@ -0,0 +1,16 @@
+package steprepository
+
+import (
+	stepdto "github.com/Farhan1033/resep-masakan-monolith.git/internal/recipe_steps_module/dto"
+	stepentity "github.com/Farhan1033/resep-masakan-monolith.git/internal/recipe_steps_module/entity"
+	"github.com/Farhan1033/resep-masakan-monolith.git/pkg/errs"
+	"github.com/google/uuid"
+)
+
+type RecipeStepRepository interface {
+	Create(step *stepentity.RecipeStep) (*stepentity.RecipeStep, errs.ErrMessage)
+	Get() ([]stepentity.RecipeStep, errs.ErrMessage)
+	Update(id uint, dto *stepdto.UpdateRequest) errs.ErrMessage
+	Delete(id uint) errs.ErrMessage
+	GetMaxStepNumberByRecipe(recipeId uuid.UUID) (int, errs.ErrMessage)
+}
