c4848ed378ae7692e4ea7972da2789913c2dabe2
Farhan1033 <hanzoi1006@gmail.com>
2025-10-04 01:28:52 +0700

    Perubahan GORM query select di detail_recipe repository

--- internal/detail_recipe_module/repository/detail_recipe_repository/repository.go
+++ internal/detail_recipe_module/repository/detail_recipe_repository/repository.go
@@ -9,8 +9,8 @@ import (
 
 type DetailRecipeRepository interface {
 	Get() ([]dto.RecipeWithIngredients, errs.ErrMessage)
-	GetById(id uuid.UUID) (*dto.RecipeWithIngredients, errs.ErrMessage)
-	GetByRecipeId(idRecipe uuid.UUID) (*dto.RecipeWithIngredients, errs.ErrMessage)
+	GetById(id uuid.UUID) (*detailrecipeentity.DetailRecipeEntity, errs.ErrMessage)
+	GetByRecipeId(idRecipe uuid.UUID) ([]dto.RecipeWithIngredients, errs.ErrMessage)
 	Create(detail *detailrecipeentity.DetailRecipeEntity) (*detailrecipeentity.DetailRecipeEntity, errs.ErrMessage)
 	Update(id uuid.UUID, detail *detailrecipeentity.DetailRecipeEntity) errs.ErrMessage
 	Delete(id uuid.UUID) errs.ErrMessage
