af2af066e6b212d12c8216a534d7d2c4012ae601
Farhan1033 <hanzoi1006@gmail.com>
2025-10-04 01:29:05 +0700

    Perubahan GORM query select di detail_recipe repository

--- internal/detail_recipe_module/repository/detail_recipe_repository_pg/repository_pg.go
+++ internal/detail_recipe_module/repository/detail_recipe_repository_pg/repository_pg.go
@@ -34,24 +34,13 @@ func (r *DetailRecipeRepo) Get() ([]dto.RecipeWithIngredients, errs.ErrMessage) 
 	if err := r.db.Table("detail_recipes AS ri").
 		Select(`
             ri.id,
-            r.id AS recipe_id,
-            r.title AS recipe_title,
-            r.description AS recipe_description,
-            r.difficult_level AS difficult_level,
-            r.prep_time AS prep_time,
-            r.cook_time AS cook_time,
-            r.total_time AS total_time,
-            r.servings AS servings,
-            r.origin_region AS origin_region,
-            r.image_url AS image_url,
-            r.is_active AS is_active,
+            ri.recipe_id AS recipe_id,
             i.id AS ingredient_id,
             i.name AS ingredient_name,
             ri.quantity,
             ri.unit,
 			ri.created_by
         `).
-		Joins("JOIN recipe r ON r.id = ri.recipe_id").
 		Joins("JOIN ingredient i ON i.id = ri.ingredient_id").
 		Scan(&results).Error; err != nil {
 
@@ -64,69 +53,34 @@ func (r *DetailRecipeRepo) Get() ([]dto.RecipeWithIngredients, errs.ErrMessage) 
 	return results, nil
 }
 
-func (r *DetailRecipeRepo) GetById(id uuid.UUID) (*dto.RecipeWithIngredients, errs.ErrMessage) {
-	var result dto.RecipeWithIngredients
+func (r *DetailRecipeRepo) GetById(id uuid.UUID) (*detailrecipeentity.DetailRecipeEntity, errs.ErrMessage) {
+	var result *detailrecipeentity.DetailRecipeEntity
 
-	if err := r.db.Table("detail_recipes AS ri").
-		Select(`
-            ri.id,
-            r.id AS recipe_id,
-            r.title AS recipe_title,
-            r.description AS recipe_description,
-            r.difficult_level AS difficult_level,
-            r.prep_time AS prep_time,
-            r.cook_time AS cook_time,
-            r.total_time AS total_time,
-            r.servings AS servings,
-            r.origin_region AS origin_region,
-            r.image_url AS image_url,
-            r.is_active AS is_active,
-            i.id AS ingredient_id,
-            i.name AS ingredient_name,
-            ri.quantity,
-            ri.unit,
-			ri.created_by
-        `).
-		Joins("JOIN recipe r ON r.id = ri.recipe_id").
-		Joins("JOIN ingredient i ON i.id = ri.ingredient_id").
-		Where("ri.id = ?", id).
-		Scan(&result).Error; err != nil {
-
+	if err := r.db.First(&result, "id = ?", id).Error; err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return nil, errs.NewNotFound("Detail recipe not found!")
+			return nil, errs.NewNotFound("Data not found!")
 		}
-		return nil, errs.NewInternalServerError(err.Error())
+		return nil, errs.NewFound(err.Error())
 	}
 
-	return &result, nil
+	return result, nil
 }
 
-func (r *DetailRecipeRepo) GetByRecipeId(idRecipe uuid.UUID) (*dto.RecipeWithIngredients, errs.ErrMessage) {
-	var result dto.RecipeWithIngredients
+func (r *DetailRecipeRepo) GetByRecipeId(idRecipe uuid.UUID) ([]dto.RecipeWithIngredients, errs.ErrMessage) {
+	var result []dto.RecipeWithIngredients
 
 	if err := r.db.Table("detail_recipes AS ri").
 		Select(`
             ri.id,
-            r.id AS recipe_id,
-            r.title AS recipe_title,
-            r.description AS recipe_description,
-            r.difficult_level AS difficult_level,
-            r.prep_time AS prep_time,
-            r.cook_time AS cook_time,
-            r.total_time AS total_time,
-            r.servings AS servings,
-            r.origin_region AS origin_region,
-            r.image_url AS image_url,
-            r.is_active AS is_active,
+            ri.recipe_id AS recipe_id,
             i.id AS ingredient_id,
             i.name AS ingredient_name,
             ri.quantity,
             ri.unit,
 			ri.created_by
         `).
-		Joins("JOIN recipe r ON r.id = ri.recipe_id").
 		Joins("JOIN ingredient i ON i.id = ri.ingredient_id").
-		Where("r.id = ?", idRecipe).
+		Where("ri.recipe_id = ?", idRecipe).
 		Scan(&result).Error; err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return nil, errs.NewNotFound("Recipe not found!")
@@ -134,7 +88,7 @@ func (r *DetailRecipeRepo) GetByRecipeId(idRecipe uuid.UUID) (*dto.RecipeWithIng
 		return nil, errs.NewInternalServerError(err.Error())
 	}
 
-	return &result, nil
+	return result, nil
 }
 
 func (r *DetailRecipeRepo) Update(id uuid.UUID, detail *detailrecipeentity.DetailRecipeEntity) errs.ErrMessage {
